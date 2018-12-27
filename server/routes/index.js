e1b4132948cba0e60990e65cd5ca672006c6404a
Andrei <andrei_z@ukr.net>
2018-12-27 17:35:18 +0200

    add a Procfile

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,7 +6,7 @@ const homeRoute = (app) => {
 
     app.get('/api/', (req, res) => {
         res.status(200).json({
-            message: 'Welcome to Project Support',
+            message: 'Welcome to the Audition Project',
         });
     });
 };
