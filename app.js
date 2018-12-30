2b31a1d2a475882573de175dab2fa40496f2a166
Andrei <andrei_z@ukr.net>
2018-12-30 20:39:23 +0200

    add cors handler

--- app.js
+++ app.js
@@ -7,13 +7,29 @@ const cors = require('cors');
 const mainRoutes = require('./server/routes/main');
 const homeRoute = require('./server/routes/index');
 
+const allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+};
+
 // set up dependencies
 const app = express();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(logger('dev'));
 app.use(cors());
-app.options('*', cors()); 
+app.options('*', cors());
+app.use(allowCrossDomain); 
 require('dotenv').config();
 
 // set up mongoose
