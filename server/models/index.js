6d4cdb1e2d11be449304c1e42db0b9a62d1fa8ca
Andrei <andrei_z@ukr.net>
2018-12-31 10:45:32 +0200

    change process env

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://projectsupport:q123456@ds235732.mlab.com:35732/projectsupport',{ useNewUrlParser: true }).then(() => {  console.log('Successfully connected to data base "usersdb"');}, err => {
+mongoose.connect(`${process.env.MONGOLAB_URI}`,{ useNewUrlParser: true }).then(() => {  console.log('Successfully connected to data base "usersdb"');}, err => {
   console.log('Unsuccessfully connected to data base "usersdb"');
   if (err) throw err;
 });
